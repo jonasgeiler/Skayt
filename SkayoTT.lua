573969ce9a634524128a3d5e367335bfb767f0d2
Skayo <jonasg.cool@gmail.com>
2018-06-03 16:27:58 +0200

    Initial Commit

---
+++ SkayoTT.lua
@@ -0,0 +1,317 @@
+-- Protocol Constants --
+protocol = {}
+
+-- Commands
+protocol.CONNECT     = 0x01
+protocol.CONNACK     = 0x02
+protocol.PUBLISH     = 0x03
+protocol.PUBACK      = 0x04
+protocol.SUBSCRIBE   = 0x05
+protocol.SUBACK      = 0x06
+protocol.UNSUBSCRIBE = 0x07
+protocol.UNSUBACK    = 0x08
+protocol.DISCONNECT  = 0x09
+protocol.SUBMSG      = 0x0b -- Not 0x0a because 0x0a is a newline character and widely used
+
+-- Breakpoints
+protocol.PUBLISH_ENDTOPIC     = 0x1a
+protocol.PUBLISH_ENDMSG       = 0x1b
+protocol.SUBSCRIBE_ENDTOPIC   = 0x1c
+protocol.UNSUBSCRIBE_ENDTOPIC = 0x1d
+protocol.SUBMSG_ENDTOPIC      = 0x1e
+protocol.SUBMSG_ENDMSG        = 0x1f
+
+-- Other
+protocol.ZERO = 0x00
+
+
+
+-- SkayoTT-Server --
+pcall(function() SkayoTT_Server:close() end) -- Close old server if still open
+
+SkayoTT_Server = net.createServer()
+
+clients = {}
+
+SkayoTT_Server:listen(1337, function(conn)
+    conn:on("connection", function(client, payload)
+        print("Connection")
+    end)
+
+    conn:on("receive", function(client, payload)
+        local hex = parsePayload(payload)
+        
+        if hex[1] == protocol.CONNECT then
+            connect(client)
+        elseif hex[1] == protocol.DISCONNECT then
+            disconnect(client)
+        elseif hex[1] == protocol.SUBSCRIBE then
+            if hex[#hex] == protocol.SUBSCRIBE_ENDTOPIC then
+                subscribe(client, hex)
+            end
+        elseif hex[1] == protocol.PUBLISH then
+            if hex[#hex] == protocol.PUBLISH_ENDMSG then
+                publish(client, hex)
+            end
+        elseif hex[1] == protocol.UNSUBSCRIBE then
+            if hex[#hex] == protocol.UNSUBSCRIBE_ENDTOPIC then
+                unsubscribe(client, hex)
+            end
+        else 
+            print("Unknown Command.")
+            print("Sending: ", protocol.ZERO)
+            client:send(protocol.ZERO)
+        end
+    end)
+    
+    conn:on("sent", function(client)
+        print("Sent.")
+    end)
+end)
+
+function parsePayload(payload)
+    hex = {}
+    out = ""
+    
+    for c=1,#payload do
+        local char = payload:sub(c,c)
+        local charNum = string.byte(char)
+        hex[#hex+1] = charNum
+        out = out .. encoder.toHex(char) .. " "
+    end
+
+    print("\nReceive:", out)
+    return hex
+end
+
+function hexToString(hex)
+    str = ""
+    
+    for _,char in pairs(hex) do
+        str = str .. string.char(char)
+    end
+
+    return str
+end
+
+function getID(client)
+    local port, ip = client:getpeer()
+    return ip..':'..port
+end
+
+function hasSubscribed(client, topic)
+    for _,subTopic in pairs(client.subscribed) do
+        if subTopic == topic then
+            return true
+        end
+    end
+
+    return false
+end
+
+function newClient(client)
+    local id = getID(client)
+    
+    print("New client: ", id)
+
+    local newClient = {}
+    newClient.id = id
+    newClient.buffer = ""
+    newClient.subscribed = {}
+    newClient.instance = client
+
+    clients[#clients+1] = newClient
+
+    return true
+end
+
+function removeClient(client)
+    local id = getID(client)
+
+    for num,c in pairs(clients) do
+        if c.id == id then
+            print("Remove client: ", id)
+            clients[num] = nil
+            return true
+        end
+    end
+
+    return false
+end
+
+function subscribeClient(client, topic)
+    local id = getID(client)
+
+    for num,c in pairs(clients) do
+        if c.id == id then
+            print("Subscribe client", id, "to topic", topic)
+            table.insert(clients[num].subscribed, topic)
+            return true
+        end
+    end
+
+    return false
+end
+
+function publishClient(client, topic, msg)
+    local id = getID(client)
+
+    buffer = ""
+    buffer = buffer .. string.char(protocol.SUBMSG)
+    buffer = buffer .. topic
+    buffer = buffer .. string.char(protocol.SUBMSG_ENDTOPIC)
+    buffer = buffer .. msg
+    buffer = buffer .. string.char(protocol.SUBMSG_ENDMSG)
+
+    for num,c in pairs(clients) do
+        if hasSubscribed(clients[num], topic) then
+            -- Check if the client is still connected
+            if clients[num].instance and clients[num].instance:getpeer() then
+                if clients[num].id ~= id then
+                    print("Sending message to ", clients[num].id)
+                    clients[num].instance:send(buffer)
+                end
+            else
+                -- Remove/Disconnect the client
+                clients[num] = nil
+            end
+        end
+    end
+
+    return true
+end
+
+function unsubscribeClient(client, topic)
+    local id = getID(client)
+
+    for num,c in pairs(clients) do
+        if c.id == id then
+            local removed = false
+
+            for num,sub in pairs(clients[num].subscribed) do
+                if sub == topic then
+                    print("Unubscribe client", id, "from topic", topic)
+                    table.remove(clients[num].subscribed, num)
+                    removed = true
+                end
+            end
+
+            return removed
+        end
+    end
+
+    return false
+end
+
+function connect(client)
+    print("CMD: Connect")
+
+    success = newClient(client)
+
+    if success then
+        print("Connection accepted.")
+        print("Sending: ", protocol.CONNACK)
+        client:send(protocol.CONNACK)
+    else
+        print("Connection declined.")
+        print("Sending: ", protocol.ZERO)
+        client:send(protocol.ZERO)
+    end
+end
+
+function disconnect(client)
+    print("CMD: Disconnect")
+
+    success = removeClient(client)
+
+    if success then
+        print("Successfully Disconnected.")
+    else 
+        print("Error while disconnecting")
+    end
+end
+
+function subscribe(client, payload)
+    print("CMD: Subscribe")
+
+    payload[1] = nil
+    payload[#payload] = nil
+
+    local topic = hexToString(payload)
+
+    print("Topic: ", topic)
+
+    success = subscribeClient(client, topic)
+
+    if success then
+        print("Successfully subscribed!")
+        print("Sending: ", protocol.SUBACK)
+        client:send(protocol.SUBACK)
+    else
+        print("Subscribe failed.")
+        print("Sending: ", protocol.ZERO)
+        client:send(protocol.ZERO)
+    end
+end
+
+function publish(client, payload)
+    print("CMD: Publish")
+
+    payload[1] = nil
+    payload[#payload] = nil
+
+    local topic, msg = "", ""
+
+    local topicEnd = false
+    for _,char in pairs(payload) do
+        if char == protocol.PUBLISH_ENDTOPIC then
+            topicEnd = true
+        else
+            if not topicEnd then
+                topic = topic .. string.char(char)
+            else
+                msg = msg .. string.char(char)
+            end
+        end
+    end
+
+    print("Topic: ", topic)
+    print("Message: ", msg)
+
+    success = publishClient(client, topic, msg)
+
+    if success then
+        print("Successfully published!")
+        print("Sending: ", protocol.PUBACK)
+        client:send(protocol.PUBACK)
+    else
+        print("Publish failed.")
+        print("Sending: ", protocol.ZERO)
+        client:send(protocol.ZERO)
+    end
+end
+
+function unsubscribe(client, payload)
+    print("CMD: Unsubscribe")
+
+    payload[1] = nil
+    payload[#payload] = nil
+
+    local topic = hexToString(payload)
+
+    print("Topic: ", topic)
+
+    success = unsubscribeClient(client, topic)
+
+    if success then
+        print("Successfully unsubscribed!")
+        print("Sending: ", protocol.UNSUBACK)
+        client:send(protocol.UNSUBACK)
+    else
+        print("Unsubscribe failed.")
+        print("Sending: ", protocol.ZERO)
+        client:send(protocol.ZERO)
+    end
+end
+
+
---
+++ clients/PHP/SkayoTT.php
@@ -0,0 +1,202 @@
+<?php
+
+define('p_CONNECT',     0x01);
+define('p_CONNACK',     0x02);
+define('p_PUBLISH',     0x03);
+define('p_PUBACK',      0x04);
+define('p_SUBSCRIBE',   0x05);
+define('p_SUBACK',      0x06);
+define('p_UNSUBSCRIBE', 0x07);
+define('p_UNSUBACK',    0x08);
+define('p_DISCONNECT',  0x09);
+define('p_SUBMSG',      0x0b);
+
+define('p_PUBLISH_ENDTOPIC', 0x1a);
+define('p_PUBLISH_ENDMSG', 0x1b);
+define('p_SUBSCRIBE_ENDTOPIC', 0x1c);
+define('p_UNSUBSCRIBE_ENDTOPIC', 0x1d);
+define('p_SUBMSG_ENDTOPIC', 0x1e);
+define('p_SUBMSG_ENDMSG', 0x1f);
+
+define('p_ZERO', 0x00);
+
+class SkayoTT {
+	public $address;
+	public $post;
+
+	private $socket;
+
+	function __construct($address, $port, $debug = false) {
+		$this->address = $address;
+		$this->port = $port;
+
+		$this->socket = stream_socket_client("tcp://$address:$port", $errno, $errstr, 60, STREAM_CLIENT_CONNECT);
+
+		if (!$this->socket) {
+			error_log("stream_socket_create() $errno, $errstr \n");
+			return false;
+		}
+
+		stream_set_timeout($this->socket, 5);
+		stream_set_blocking($this->socket, 0);
+
+		register_shutdown_function([$this, 'disconnect']);
+		set_error_handler([$this, 'disconnectError']);
+
+		$this->debug = $debug;
+	}
+
+	private function read($int = 8192, $nb = false){
+		$string = "";
+		$togo = $int;
+		
+		if($nb){
+			return fread($this->socket, $togo);
+		}
+			
+		while (!feof($this->socket) && $togo>0) {
+			$fread = fread($this->socket, $togo);
+			$string .= $fread;
+			$togo = $int - strlen($string);
+		}
+		
+		return $string;
+	}
+
+	public function connect() {
+		$buffer = "";
+		$buffer .= chr(p_CONNECT);
+
+		fwrite($this->socket, $buffer);
+
+		if($this->read(1) == p_CONNACK) {
+			if($this->debug) echo 'Connection accepted!' . PHP_EOL;
+			return true;
+		} else {
+			if($this->debug) echo 'Connection refused.' . PHP_EOL;
+			return false;
+		}
+	}
+
+	public function disconnect() {
+		$buffer = "";
+		$buffer .= chr(p_DISCONNECT);
+
+		fwrite($this->socket, $buffer);
+		stream_socket_shutdown($this->socket, STREAM_SHUT_WR);
+
+		if($this->debug) echo 'Disconnected.' . PHP_EOL;
+	}
+
+	public function disconnectError($errno, $errstr, $errfile, $errline) {
+		echo "$errstr in $errfile on line $errline";
+		$this->disconnect();
+	}
+
+	public function subscribe($topic) {
+		$buffer = "";
+		$buffer .= chr(p_SUBSCRIBE);
+		$buffer .= $topic;
+		$buffer .= chr(p_SUBSCRIBE_ENDTOPIC);
+
+		fwrite($this->socket, $buffer);
+
+		if($this->read(1) == p_SUBACK) {
+			if($this->debug) echo 'Subscribe successful!' . PHP_EOL;
+			return true;
+		} else {
+			if($this->debug) echo 'Subscribe failed.' . PHP_EOL;
+			return false;
+		}
+	}
+
+	public function publish($topic, $msg) {
+		$buffer = "";
+		$buffer .= chr(p_PUBLISH);
+		$buffer .= $topic;
+		$buffer .= chr(p_PUBLISH_ENDTOPIC);
+		$buffer .= $msg;
+		$buffer .= chr(p_PUBLISH_ENDMSG);
+
+		fwrite($this->socket, $buffer);
+
+		if($this->read(1) == p_PUBACK) {
+			if($this->debug) echo 'Publish successful!' . PHP_EOL;
+			return true;
+		} else {
+			if($this->debug) echo 'Publish failed.' . PHP_EOL;
+			return false;
+		}
+	}
+
+	public function unsubscribe($topic) {
+		$buffer = "";
+		$buffer .= chr(p_UNSUBSCRIBE);
+		$buffer .= $topic;
+		$buffer .= chr(p_UNSUBSCRIBE_ENDTOPIC);
+
+		fwrite($this->socket, $buffer);
+
+		if($this->read(1) == p_UNSUBACK) {
+			if($this->debug) echo 'Unsubscribe successful!' . PHP_EOL;
+			return true;
+		} else {
+			if($this->debug) echo 'Unsubscribe failed.' . PHP_EOL;
+			return false;
+		}
+	}
+
+	public function checkForMessage() {
+		$rawMessage = $this->read(8192, true);
+
+		$message = "";
+		$topic = "";
+		$topicEnd = false;
+		for($i = 0; $i < strlen($rawMessage); $i++) {
+			if(ord($rawMessage[$i]) == p_SUBMSG_ENDTOPIC) {
+				$topicEnd = true;
+				continue;
+			}
+
+			if(ord($rawMessage[$i]) == p_SUBMSG_ENDMSG)
+				break;
+
+			if(ord($rawMessage[$i]) == p_SUBMSG)
+				continue;
+
+			if(!$topicEnd) {
+				$topic .= $rawMessage[$i];
+			} else {
+				$message .= $rawMessage[$i];
+			}
+		}
+
+		if($topic && $message) 
+			return ['msg' => $message, 'topic' => $topic];
+
+		return false;
+	}
+}
+
+// DEMO - Uncomment to test it
+/*
+$address = '192.168.1.110';
+$port = 1337;
+
+$skayott = new SkayoTT($address, $port, true);
+$skayott->connect();
+$skayott->subscribe('/lights');
+$skayott->publish('/lights', 'ON');
+while(true) {
+	$message = $skayott->checkForMessage();
+
+	if($message) {
+		echo "Received '" . $message['msg'] . "' from " . $message['topic'] . PHP_EOL;
+	}
+
+	if($message == 'ON' && $topic == '/lights') {
+		$skayott->unsubscribe('/lights');
+		exit();
+	}
+}
+*/
